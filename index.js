14d3409743cf1a4c8d61055f5698646663149a4f
christina <chrstnaheadley@gmail.com>
2021-04-18 21:56:40 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/* global Vue */
+/* global axios */
+
+// var app = new Vue({
+//   el: "#app",
+//   data: {
+//     vues: "",
+//   },
+//   created: function () {
+//     this.vueInfo();
+//   },
+//   methods: {
+//     vueInfo: function () {
+//       axios.get("http://localhost:3000/api/vues").then((response) => {
+//         this.vues = response.data;
+//         console.log(this.vues);
+//       });
+//     },
+//   },
+// });
